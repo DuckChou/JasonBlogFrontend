1bd1e82ed082fd859a3b031ba62a2e1c539cbc8a
Keren Zhou <jason.keren.zhou@outlook.com>
2023-12-21 17:00:16 +1100

    Add margin to footer

--- src/components/view/Footer.js
+++ src/components/view/Footer.js
@@ -3,7 +3,7 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <footer>
-      <p className="text-center text-success foot-slogan">
+      <p className="text-center text-success foot-slogan mt-3">
         Where there is a will, there is a way.
       </p>
       <p className="text-center">Copyright 2023 © Keren Zhou (Jason)</p>
