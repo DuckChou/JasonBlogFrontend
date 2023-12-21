c66e4bfe6fd4325e1bccbf340b2d99dd41c28f0a
Keren Zhou <jason.keren.zhou@outlook.com>
2023-12-21 11:52:11 +1100

    Add a footer words

--- src/components/view/Footer.js
+++ src/components/view/Footer.js
@@ -1,7 +1,12 @@
+import "./Footer.css";
+
 export default function Footer() {
   return (
     <footer>
-      <p className="text-center my-5">Copyright 2023 © Keren Zhou (Jason)</p>
+      <p className="text-center text-success foot-slogan">
+        Where there is a will, there is a way.
+      </p>
+      <p className="text-center">Copyright 2023 © Keren Zhou (Jason)</p>
     </footer>
   );
 }
